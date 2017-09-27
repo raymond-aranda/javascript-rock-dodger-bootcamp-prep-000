4813cd6c613a594390d974aba395f9b61a3ed776
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-27 02:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function endGame() {
   START.innerHTML = 'Play again?'
   START.style.display = 'inline'
 
-  return alert('YOU LOSE!');
+  return alert('YOU LOSE!')
 }
 
 function moveDodger(e) {
